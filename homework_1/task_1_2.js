9248cb1dbf3b9dd92ce1cf152a2ce4cc5a7552ed
Taisiya Kuznetsova <taiskuz@mail.ru>
2018-08-07 07:55:57 +0300

    hw-1 tsk-1 tsk-2

---
+++ homework_1/task_1_2.js
@@ -0,0 +1,110 @@
+'use strict';
+
+// JS-level-2
+// homework-1
+// task-1, task-2
+
+// базовый класс Container
+class Container {
+    constructor(id, className, htmlCode) {
+        this.name = name;
+        this.id = id;
+        this.className = className;
+        this.htmlCode = htmlCode;
+    }
+
+    render() {
+        return this.htmlCode;
+    }
+};
+
+// task-1
+// Улучшить базовый класс, добавив в него общий для всех метод remove(),
+// который удаляет контейнер.
+Container.prototype.remove = function() { this.htmlCode = "" };
+
+class Menu extends Container {
+    constructor(id, className, menuTitle, items) {
+        super();
+        this.id = id;
+        this.className = className;
+        this.menuTitle = menuTitle;
+        this.items = items;
+    }
+
+    render() {
+        if (this.items.lenght == 0) return;
+
+        let menuList = `<ul class="${this.className}" id="${this.id}">`;
+
+        for (let item in this.items) {
+            if (this.items[item] instanceof MenuItem) {
+                menuList += this.items[item].render();
+            }
+        }
+        menuList += `</ul>`;
+        return menuList;
+    };
+};
+
+class MenuItem extends Menu {
+    constructor(href, itemTitle) {
+        super();
+        this.className = "menu-item";
+        this.href = href;
+        this.itemTitle = itemTitle;
+    }
+
+    render() {
+        return `<li class="${this.className}" href="${this.href}">${this.itemTitle}</li>`;
+    }
+
+};
+
+let menuItem1_1 = new MenuItem("/", "Главная");
+let menuItem1_2 = new MenuItem("/catalogue/", "Каталог");
+let menuItem1_3 = new MenuItem("/gallery/", "Галерея");
+let menuItems1 = { 0: menuItem1_1, 1: menuItem1_2, 2: menuItem1_3 };
+
+// task-2
+// Создать наследника класса Menu – новый класс должен уметь
+// строить меню со вложенными пунктами, т.е с подменю.
+// Подсказка: главный секрет в обходе объекта пунктов меню и проверке типов.
+class MenuWithSubItems extends Menu {
+    constructor(id, className, menuItems) {
+        super();
+        this.id = id;
+        this.className = className;
+        this.menuItems = menuItems;
+    }
+
+    render() {
+        if (this.menuItems.lenght == 0) return;
+
+        let menuWithSubItems = `<ul class="${this.className}" id="${this.id}">`;
+
+        for (let menuItem in this.menuItems) {
+            menuWithSubItems += `<li class="${menuItem.className}">${this.menuItems[menuItem].menuTitle}`;
+
+            if (this.menuItems[menuItem] instanceof Menu) {
+                menuWithSubItems += this.menuItems[menuItem].render();
+            }
+            menuWithSubItems += `</li>`;
+        }
+
+        menuWithSubItems += `</ul>`;
+        return menuWithSubItems;
+    }
+};
+
+let menuItem2_1 = new MenuItem("/location/", "Адрес");
+let menuItem2_2 = new MenuItem("/about/", "О компании");
+let menuItems2 = { 0: menuItem2_1, 1: menuItem2_2 };
+
+let submenu1 = new Menu("submenu-1", "submenu-item", "Главная", menuItems1);
+let submenu2 = new Menu("submenu-2", "submenu-item", "О компании", menuItems2);
+let menuItems = { 0: submenu1, 1: submenu2 };
+
+let menuWithSubItems = new MenuWithSubItems("menu-with-submenus", "menu-with-submenus", menuItems);
+
+let div = document.write(menuWithSubItems.render());
---
+++ homework_1/task_3.js
@@ -0,0 +1,78 @@
+'use strict';
+
+// [WIP] this task is in progress
+
+// JS-level-2
+// homework-1
+// task-3
+
+// task-3 *
+// * Некая сеть фаст фудов предлагает несколько видов гамбургеров:
+//   ● маленький (50 рублей, 20 калорий);
+//   ● большой (100 рублей, 40 калорий).
+
+// Гамбургер может быть с одним из нескольких видов начинок(обязательно):
+//   ● сыром(+ 10 рублей, + 20 калорий);
+//   ● салатом(+ 20 рублей, + 5 калорий);
+//   ● картофелем(+ 15 рублей, + 10 калорий)
+
+// Дополнительно, гамбургер можно
+//   ● посыпать приправой(+ 15 рублей, 0 калорий)
+//   ● и полить майонезом(+ 20 рублей, + 5 калорий).
+
+// Напишите программу, рассчитывающую стоимость и калорийность гамбургера.
+// Используйте ООП подход (подсказка:
+// нужен класс Гамбургер, константы, методы для выбора опций и расчета нужных величин).
+
+class Hamburger {
+    constructor(size, stuffing) {
+        this.size = size;
+        this.stuffing = stuffing;
+        this.toppings = [];
+    }
+
+    // Добавить добавку к гамбургеру. Можно добавить несколько
+    // добавок, при условии, что они разные.
+    set toppings(toppings) {
+        if (toppings.lenght == 0) return;
+
+        let toppingsList = [];
+        for (let topping in toppings) {
+            for (let index in this.toppings) {
+                if (toppings[topping] !== this.toppings[index]) {
+                    toppingsList.push(toppings[topping]);
+                }
+            }
+        }
+        [this.toppings] = toppingsList;
+    };
+
+    removeToppings(toppings) {
+        if (toppings.lenght == 0 || this.toppings.lenght == 0) return;
+
+        let toppingsList = this.toppings;
+        for (let topping in toppings) {
+            for (let index in this.toppings) {
+                if (toppings[topping] === this.toppings[index]) {
+                    toppingsList.splice(index, index);
+                }
+            }
+        }
+        [this.toppings] = toppingsList;
+    }
+};
+
+//  Размеры, виды начинок и добавок
+const SIZE_SMALL = { type: "size", priсe: 50, calories: 20 };
+const SIZE_LARGE = { type: "size", priсe: 100, calories: 20 };
+const STUFFING_CHEESE = { type: "stuff", priсe: 10, calories: 20 };
+const STUFFING_SALAD = { type: "stuff", priсe: 20, calories: 5 };
+const STUFFING_POTATO = { type: "stuff", priсe: 15, calories: 10 };
+const TOPPING_SPICE = { type: "topping", priсe: 15, calories: 0 };
+const TOPPING_MAYO = { type: "topping", priсe: 20, calories: 5 };
+
+const MyHamburger = new Hamburger(SIZE_SMALL, [STUFFING_CHEESE, STUFFING_POTATO]);
+MyHamburger.toppings = [TOPPING_SPICE, TOPPING_MAYO];
+MyHamburger.removeToppings([TOPPING_SPICE]);
+
+console.log('MyHamburger ', MyHamburger);
